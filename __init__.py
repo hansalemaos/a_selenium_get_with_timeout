3410328e8988f164960cb31dd0093b4cb10382a3
Hans Alemão <aulasparticularesdealemaosp@gmail.com>
2023-01-01 14:42:36 -0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -12,30 +12,17 @@ def _format_url_for_selenium(url):
     return wholelink
 
 
-def get_site_with_timeout(driver, url, timeout=5, ready_state=True):
+def get_site_with_timeout(driver, url, timeout=5):
     url = _format_url_for_selenium(url)
-    siteloaded = False
     oldvalue = driver.__dict__["caps"]["timeouts"]["script"]
     try:
 
         driver.set_script_timeout(timeout)
-        finaltime = time() + timeout + 0.5
-        while time() < finaltime:
-            try:
-                driver.execute_script(f"window.location.href='{url}'")
-                if not ready_state:
-                    siteloaded = True
+        driver.execute_async_script(f"window.location.href='{url}'")
+    except Exception as b:
+        pass
 
-                break
-            except Exception as F:
-                continue
-
-        if ready_state:
-            while time() < finaltime and siteloaded is False:
-                siteloaded = (
-                    driver.execute_script("return document.readyState") == "complete"
-                )
-                sleep(0.01)
     finally:
+        if timeout > 0:
+            sleep(timeout)
         driver.set_script_timeout(oldvalue)
-    return siteloaded
